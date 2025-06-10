8d487aa5c554244684bebc6e4734197b62b740de
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2025-06-10 11:22:08 -0300

    Add files via upload

---
+++ agenda 2/js/script.js
@@ -0,0 +1,115 @@
+const addBtn = document.getElementById("adicionar-btn")
+const tarefaContainer = document.getElementById("tarefa-container")
+
+
+tarefas = []
+
+addBtn.addEventListener("click", function() {
+    
+    criarTarefa()
+    limparTarefas()
+    getTarefas()
+    carregarTarefas()
+    btnFunction()
+    console.log(tarefas);
+    
+})
+
+
+let criarTarefa = function() {
+    let titulo = window.prompt("Insira o título da tarefa")
+    let local = window.prompt("Insira a tarefa")
+    let data = window.prompt("Insira a data de conclusão da tarefa")
+
+    let novaTarefa = {
+        titulo: titulo,
+        local: local,
+        data: data,
+        id: sessionStorage.length
+    }
+
+    sessionStorage["tarefa" + (sessionStorage.length)] = JSON.stringify(novaTarefa)
+}
+
+const criarContainerTarefa = (id) => {
+    let div = document.createElement("div")
+    div.classList.add("tarefa")
+    div.id = id
+    tarefaContainer.appendChild(div)
+    return div
+}
+
+const criarTitulo = (div, titulo) => {
+    let h1 = document.createElement("h1")
+    h1.innerHTML = titulo
+    div.appendChild(h1)
+}
+
+const criarLocal = (div, local) => {
+    let h2 = document.createElement("h2")
+    h2.innerHTML = local
+    div.appendChild(h2)
+}
+
+const criarData = (div, data) => {
+    let h3 = document.createElement("h3")
+    h3.innerHTML = data
+    div.appendChild(h3)
+}
+
+const carregarTarefas = () => {
+    tarefas.map((tarefa) => {
+        let div = criarContainerTarefa(tarefa.id)
+        criarTitulo(div, tarefa.titulo)
+        criarLocal(div, tarefa.local)
+        criarData(div, tarefa.data)
+    })
+    btnFunction()
+}
+
+let limparTarefas = function() {
+    while(tarefaContainer.firstChild) {
+        tarefaContainer.removeChild(tarefaContainer.firstChild)
+    }
+}
+
+
+const getTarefas = () => {
+    tarefas = []
+    for(var i = 0; i < sessionStorage.length; i++) {
+        tarefas.push(JSON.parse(sessionStorage.getItem("tarefa"+i)))
+    }
+}
+
+const a = () => {
+    
+}
+
+const btnFunction = () => {
+    let a = document.querySelectorAll(".tarefa")
+    
+    a.forEach((tarefa) => {
+        if (!tarefa.dataset.listenerAdded) {
+            tarefa.addEventListener("click", function(e){
+                let titulo = window.prompt("Insira o título da tarefa")
+                let local = window.prompt("Insira a tarefa")
+                let data = window.prompt("Insira a data de conclusão da tarefa")
+
+                let novaTarefa = {
+                    titulo: titulo,
+                    local: local,
+                    data: data,
+                    id: sessionStorage.length
+                }
+                sessionStorage.setItem("tarefa"+e.target.id, JSON.stringify(novaTarefa))
+                })
+            }
+        // tarefa.dataset.listenerAdded = true;
+        getTarefas()
+        carregarTarefas()
+    })
+
+}
+
+getTarefas()
+carregarTarefas()
